558f0ce88ffc23e073e5419b3f3561b558c97eb3
lucky <kurniawnaluckyy@gmail.com>
2023-03-26 21:48:52 +0800

    add readme

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import FormField from './src/components/FormField';
+import { formData } from './src/formData';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const RegisterForm = (props) => {
+    const [formValues, handleFormValueChange, setFormValues] = formData({
+        username: '',
+        email: '',
+        password: ''
+    })
+    return (
+        <View style={styles.container}>
+            <Text style={{
+                fontSize: 24,
+                textAlign: 'center',
+                fontWeight: "300",
+                paddingBottom: 30
+            }}>Hellooww !!</Text>
+            <FormField
+                label='Username'
+                formKey='username'
+                placeholder='Your username'
+
+                handleFormValueChange={handleFormValueChange}
+            />
+            <FormField
+                label='Email'
+                formKey='email'
+                placeholder='Your email id'
+                textInputProps={{
+                    autoCapitalize: "none"
+                }}
+                handleFormValueChange={handleFormValueChange}
+            />
+            <FormField
+                label='Password'
+                formKey='password'
+                placeholder='Your password'
+                textInputProps={{
+                    autoCapitalize: "none"
+                }}
+                handleFormValueChange={handleFormValueChange}
+            />
+            <Text style={styles.header}>Values in Hook: </Text>
+            <View>
+                <Text style={styles.formText}>Username is : {formValues.username}</Text>
+                <Text style={styles.formText}>Email is: {formValues.email}</Text>
+                <Text style={styles.formText}>Password is: {formValues.password}</Text>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        display: 'flex',
+        margin: 20,
+    },
+    header: {
+        fontSize: 20,
+        paddingTop: 30
+    },
+    formText: {
+        fontSize: 20,
+        padding: 10,
+        paddingLeft: 0
+    }
+})
+
+export default RegisterForm;
---
+++ README.md
@@ -0,0 +1,23 @@
+Create Project
+---------------
+```
+npx create-expo-app FormProject
+```
+```
+cd FormProject
+```
+
+Running
+-------
+web
+```
+npm run web
+```
+ios
+```
+npm run ios
+```
+android
+```
+npm run android
+```
